3bc6702132921312286b3dfce2125bdcd61b803e
Adam Koch <me@adamko.ch>
2019-05-11 16:59:49 -0400

    Updated for BFA.

---
+++ Locales/esMX.lua
@@ -0,0 +1,8 @@
+-------------------------------------------------------------------------------
+-- ElvUI Improved System Datatext By Lockslap (US, Bleeding Hollow)
+--------------------------------------------------------------------------------
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("ElvUI_SystemDT", "esMX", false)
+if not L then return end
+
+--@localization(locale="esMX", format="lua_additive_table", same-key-is-true=true, escape-non-ascii=true, handle-unlocalized="english")@
